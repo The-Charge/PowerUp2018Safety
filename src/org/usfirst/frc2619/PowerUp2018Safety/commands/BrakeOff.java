3336f9250c3adff836edc0f57ca53fc98a837362
Nathaniel Kalantar <NathanielK392@gmail.com>
2018-07-16 19:53:45 -0400

    Mistake - was using old PowerUp2018. Pulled from new branch and updated the .yaml.

--- src/org/usfirst/frc2619/PowerUp2018Safety/commands/BrakeOff.java
+++ src/org/usfirst/frc2619/PowerUp2018Safety/commands/BrakeOff.java
@@ -38,6 +38,7 @@ public class BrakeOff extends InstantCommand {
     // Called once when this command runs
     @Override
     protected void initialize() {
+    	Robot.elevator.brakeOff();
     }
 
 }
